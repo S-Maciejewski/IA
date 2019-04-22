d6b897040e992cbfa31ca52e1daed570c7dd129c
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-04-22 17:06:20 +0200

    Proper notes displaying

--- notatnik/script.js
+++ notatnik/script.js
@@ -25,6 +25,7 @@ function saveData(data) {
     console.log('Data from server response saved');
     console.log('Notes: ', notes);
     console.log('Categories: ', categories);
+    refreshNotes();
 }
 
 function init() {
@@ -37,11 +38,58 @@ function init() {
 }
 
 function loginVisibility(success) {
-    if (success){
+    if (success) {
         $('#loginField').css('visibility', 'hidden');
         $('#loginField label').css('visibility', 'hidden');
     }
     loggedIn = success;
+    refreshNotes();
+}
+
+function refreshNotes() {
+    $('#notesField').html(`
+    <ul class="mdl-list" id="notesList">
+        <li class="mdl-list__item mdl-list__item--two-line">
+            <span class="mdl-list__item-primary-content">
+                Notes list
+            </span>
+        </li>
+    </ul>
+    `);
+    for (i in notes) {
+        var listElement = $('#notesList');
+        console.log('logged in status:', loggedIn);
+        if (!loggedIn)
+            $(listElement).append(`
+            <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+                <i class="material-icons mdl-list__item-icon">note</i>
+                `
+                + notes[i].title +
+                `
+            </span>
+            </li>
+            `);
+        else
+            $(listElement).append(`
+            <li class="mdl-list__item mdl-list__item--two-line">
+            <span class="mdl-list__item-primary-content">
+                <i class="material-icons mdl-list__item-icon">note</i>
+                <span>`
+                + notes[i].title +
+                `</span>
+                <span class="mdl-list__item-sub-title">`
+                + notes[i].category +
+                `<br>`
+                + notes[i].date +
+                `</span>
+            </span>
+            <span class="mdl-list__item-secondary-content">`
+                + notes[i].content +
+                `</span>
+        </li>
+            `);
+    }
 }
 
 $(function () {
