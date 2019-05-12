f9c5d9b2f0aabb8e4871e4105b49e58ec8eb2c06
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-05-12 18:55:17 +0200

    Lab ASP .Net MVC

---
+++ Lab8/Lab8/Lab8/Models/MusicDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lab8.Models
+{
+    public class MusicDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+
+        public System.Data.Entity.DbSet<Lab8.Models.Genre> Genres { get; set; }
+    }
+}
